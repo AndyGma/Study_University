fc5dc533b4cccdbfe9161a0f497628a3e7bfef25
Andrew Klokov <555135@mail.ru>
2021-11-17 23:22:25 +0300

    Добавил задание

--- QualityAssurance_Task_1.py
+++ QualityAssurance_Task_1.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 
 class Task_1:
@@ -6,16 +8,33 @@ class Task_1:
 
     def InitData(self):
         if self.var == 2:  # Аня
-            self.sizeFile = 60000
-            self.sizeMTU_1 = 1410
-            self.sizeMTU_2 = 700
-            self.sizeMTU_3 = 1460
-        if self.var == 9:  # Андрей
-            self.sizeFile = 60000
-            self.sizeMTU_1 = 1410
-            self.sizeMTU_2 = 700
-            self.sizeMTU_3 = 1460
+            self.sizeFile = 60 * 10**3  # Размер файла, байт
+            self.sizeMTU_1 = 1410  # Значение MTU 1-го сегмента
+            self.sizeMTU_2 = 700  # Значение MTU 2-го сегмента
+            self.sizeMTU_3 = 1460  # Значение MTU 3-го сегмента
+        elif self.var == 9:  # Андрей
+            self.sizeFile = 200 * 10**3  # Размер файла, байт
+            self.sizeMTU_1 = 1410  # Значение MTU 1-го сегмента
+            self.sizeMTU_2 = 1350  # Значение MTU 2-го сегмента
+            self.sizeMTU_3 = 1420  # Значение MTU 3-го сегмента
+        else:
+            print("Неправильно указан номер варианта")
+            sys.exit()
+
+    def One(self):
+        self.InitData()
+
+
 
 if __name__ == "__main__":
-    Work = Task_1(2)
+    Work = Task_1(10)
     Work.One()
+
+    # Задание:
+    # Рассчитать:
+    # Обьем данных и количество пакетов информации, которые
+    #   Переданы сервером
+    #   Приняты оборудованием пользователя
+    #       без учета фазы установления и завершения TCP/IP соединения
+    # На сколько процентов скорость приема данных на стороне пользователя ...
+    #   должна быть выше скорости передачи данных сервером?
