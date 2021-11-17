4ad9238ae2125beae7cdf38066def92482073799
Andrew Klokov <555135@mail.ru>
2021-11-17 21:42:13 +0300

    Init initData

--- QualityAssurance_Task_1.py
+++ QualityAssurance_Task_1.py
@@ -1,10 +1,21 @@
-
+import numpy as np
 
 class Task_1:
     def __init__(self, var):
         self.var = var
 
-    
+    def InitData(self):
+        if self.var == 2:  # Аня
+            self.sizeFile = 60000
+            self.sizeMTU_1 = 1410
+            self.sizeMTU_2 = 700
+            self.sizeMTU_3 = 1460
+        if self.var == 9:  # Андрей
+            self.sizeFile = 60000
+            self.sizeMTU_1 = 1410
+            self.sizeMTU_2 = 700
+            self.sizeMTU_3 = 1460
 
 if __name__ == "__main__":
     Work = Task_1(2)
+    Work.One()
