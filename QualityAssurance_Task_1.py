bae0ecc8cc3fbcf69c66da3db901f3c008c5f1a7
Andrew Klokov <555135@mail.ru>
2021-11-22 01:50:04 +0300

    Добавил задание

--- QualityAssurance_Task_1.py
+++ QualityAssurance_Task_1.py
@@ -1,6 +1,7 @@
 import sys
 
 import numpy as np
+import math
 
 class Task_1:
     def __init__(self, var):
@@ -21,14 +22,42 @@ class Task_1:
             print("Неправильно указан номер варианта")
             sys.exit()
 
-    def One(self):
+    def Cycle(self):
         self.InitData()
+        self.Ask()
+
+
+    def Ask(self):
+        # Сетевой сегмент 3
+
+        MSS_3 = self.sizeMTU_3 - 40
+        print(f"\nВ начале сервер отдает пакеты в 3-й сегмент сети, где MTU = {self.sizeMTU_3} байт. Размер файла {self.sizeFile} байт."
+              f"\nСегменты файла инкапсулируются в TCP и IP (20 + 20 байт)."
+              f"\nТо есть MSS для 3-го сетевого сегмента составит MSS =  MTU - 40 = {self.sizeMTU_3 - 40} байт")
+        count_packeges_after_MTU = math.ceil(self.sizeFile / (self.sizeMTU_3 - 40))
+        remainder = self.sizeFile - math.floor(self.sizeFile / (self.sizeMTU_3 - 40)) * (self.sizeMTU_3 - 40)  # остаток
+        print(f"Файл, после прохождение через 3-й сетевой сегмент, разделится на {count_packeges_after_MTU} пакетов,"
+              f"\n\tиз которых {count_packeges_after_MTU - 1} пакет содержит 40 байт (TCP и IP адреса)"
+              f"\n\tи {MSS_3} байт передаваемого сегмента."
+              f"\n\tА оконечный пакет содержит 40 байт (TCP и IP) и {remainder} байт."
+              f"\nИтог после 3-го сетевого сегмента:"
+              f"\n\tКоличество IP пакетов:"
+              f"\n\t\t{count_packeges_after_MTU - 1} шт. каждая по {self.sizeMTU_3} байт"
+              f"\n\t\t1 шт.(конец) - {remainder} байт")
+
+        # Сетевой сегмент 2
+
+        MSS_2 = self.sizeMTU_2 - 40
+        print(f"\n\nДалее пакеты попадают во 2-ой сетевой сегмент."
+              f"\nMTU размер которого равен {self.sizeMTU_2}. И соответственно MSS (без учета IP и TCP) равен {MSS_2}."
+              f"Размер пакета, поступившего в сегмент сети больше чем MTU, следовательно он будет разбит на две части."
+              f"")
 
 
 
 if __name__ == "__main__":
     Work = Task_1(8)
-    Work.One()
+    Work.Cycle()
 
     # Задание:
     # Рассчитать:
---
+++ test.py
@@ -0,0 +1,3 @@
+import math
+
+print(math.ceil(6000000 / (450 - 40)))
